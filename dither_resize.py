d665fe6e6c50e5da39526f75d3daf483ed614cc0
Giovanni <81854205+beton-OS@users.noreply.github.com>
2024-10-29 16:49:41 +0100

    Add files via upload

--- dither_resize.py
+++ dither_resize.py
@@ -1,8 +1,10 @@
-import pillow_avif 
 from PIL import Image
 import numpy as np
 import sys
 import os
+import argparse
+
+
 
 def dither_matrix(n:int):
     if n == 1:
@@ -18,7 +20,7 @@ def dither_matrix(n:int):
 
 def get_image(src:str):
     img = Image.open(src)
-    img.thumbnail((800, 800))  # Resize the image
+    img.thumbnail(size)  # Resize the image
     img_gray = img.convert('L')  # Convert the image to grayscale
     img_arr = np.array(img_gray)  # Convert the image to a numpy array
     return img_arr * (1/255.0)  # Normalize the pixel values
@@ -39,13 +41,21 @@ def ordered_dithering(img_pixel:np.array, dither_m:np.array):
     Image.fromarray(img_pixel).convert('L').save(name+'_dithered.png', quality=70)
 
 if __name__ == '__main__':
-    n = 8
-    src = sys.argv[1]
-    name = os.path.splitext(src)[0]
-    dm = dither_matrix(n)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--nMatrix", dest = "n", default = 8, type = int, help = "diethering matrix dimention") 
+    parser.add_argument("-i", "--input", dest = "src", type = str, help = "image file name")
+    parser.add_argument("-s", "--size", dest = "edge", default = 800, type = int, help = "resizing value")
+    args = parser.parse_args()
+
+    n = args.n  # Use the value of n from the command line argument 
+    src = args.src  # Use the value of src from the command line argument
+    edge = args.edge # Use the value of edge from cla
+    name = os.path.splitext(src)[0] # Get the name only, drop the extension
+    size = (edge, edge) # Get the risizing dimensions
     img = get_image(src)
+    dm = dither_matrix(n)
     ordered_dithering(img, dm)
     print('Dithering is done \U0001F388')
 
-    
+
 
